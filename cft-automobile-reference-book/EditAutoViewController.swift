716c7873e048ac475819ea2aebd8aa358e881474
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-02-15 12:56:46 +0700

    Set view controllers

--- cft-automobile-reference-book/EditAutoViewController.swift
+++ cft-automobile-reference-book/EditAutoViewController.swift
@@ -1,20 +1,14 @@
 //
-//  ViewController.swift
+//  EditAutoViewController.swift
 //  cft-automobile-reference-book
 //
 //  Created by Тимофей Твердохлебов on 2/15/19.
 //  Copyright © 2019 Тимофей Твердохлебов. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+class EditAutoViewController: UIViewController {
+  
 }
-
---
+++ cft-automobile-reference-book/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cft-automobile-reference-book
+//
+//  Created by Тимофей Твердохлебов on 2/15/19.
+//  Copyright © 2019 Тимофей Твердохлебов. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
