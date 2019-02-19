a77d9d811231f9b6b55d70e5d37ddcab28350312
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-02-19 09:54:31 +0700

    Add picker view stuff

--- cft-automobile-reference-book/AutoViewController.swift
+++ cft-automobile-reference-book/AutoViewController.swift
@@ -9,13 +9,15 @@
 import Foundation
 import UIKit
 
-class AutoViewController: UIViewController {
+class AutoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+  
   @IBOutlet weak var header: UINavigationItem!
   @IBOutlet weak var manufacturerTextField: UITextField!
   @IBOutlet weak var modelTextField: UITextField!
   @IBOutlet weak var productionYearPickerView: UIDatePicker!
   @IBOutlet weak var typeOfBodyPickerView: UIPickerView!
   @IBOutlet weak var nameOfClassPickerView: UIPickerView!
+  @IBOutlet weak var rightBarButton: UIBarButtonItem!
   
   let typesOfBody = ["Hatchback",
                      "Sedan",
@@ -24,10 +26,18 @@ class AutoViewController: UIViewController {
                      "Coupe",
                      "Cabriolet"]
   
+  let namesOfClass = ["Compact",
+                     "Midsize",
+                     "Large"]
+  
   var auto: Auto?
   
   override func viewDidLoad() {
-    setTypeOfBodyPickerView()
+    self.typeOfBodyPickerView.delegate = self
+    self.typeOfBodyPickerView.dataSource = self
+    self.nameOfClassPickerView.delegate = self
+    self.nameOfClassPickerView.dataSource = self
+    
     if let innerAuto = auto {
       header.title = "\(innerAuto.manufacturer) \(innerAuto.model)"
       manufacturerTextField.text = innerAuto.manufacturer
@@ -36,7 +46,16 @@ class AutoViewController: UIViewController {
     }
   }
   
-  func setTypeOfBodyPickerView() {
-    
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
   }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return pickerView == typeOfBodyPickerView ? typesOfBody.count : namesOfClass.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return pickerView == typeOfBodyPickerView ? typesOfBody[row] : namesOfClass[row]
+  }
+  
 }
